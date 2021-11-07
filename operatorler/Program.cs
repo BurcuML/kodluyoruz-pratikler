73532de10e01aa0afb45fb7f00a5bbf3e852bbff
Burcu Leblebici <burcu_leblebici@hotmail.com>
2021-11-07 15:39:21 +0300

    added

---
+++ operatorler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=3;
+            int y=3;
+            y+=2;
+
+            Console.WriteLine(y);
+            y+=2;
+            Console.WriteLine(y);
+            y+=2;
+            Console.WriteLine(y);
+            x/=1;
+            Console.WriteLine(x);
+        }
+    }
+}
