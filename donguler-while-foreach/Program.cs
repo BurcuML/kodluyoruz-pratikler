d93b477f1209cfc085801828240d4668eb6524db
Burcu Leblebici <burcu_leblebici@hotmail.com>
2021-11-11 14:18:47 +0300

    added

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //while
+
+            //1 den başlayarak consoledan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp yazdıran program.
+
+          /*  Console.WriteLine("Sayı giriniz:");
+            int sayi=int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam=0;
+            while(sayac<=sayi){
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine("Toplam:" + toplam/sayi);*/
+
+            // a'dan z'ye kadar tüm harfleri console a yazdır.
+
+           /* char character= 'a';
+            while(character<'z'){
+                Console.WriteLine(character);
+                character++;
+            } */
+
+            Console.Write("***** Foreach *****");
+
+            string[] arabalar= {"BMW", "Ford", "Toyota", "Nissan"};
+
+            foreach(var araba in arabalar){
+
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
